9d324f3466b3e3f3225ab28643276305fb5425c0
Geo <gmarshall@middil.com>
2013-06-03 14:26:21 -0600

    With deleted check.

--- lib/friendly_id.rb
+++ lib/friendly_id.rb
@@ -45,7 +45,7 @@ with numeric ids:
 module FriendlyId
 
   # The current version.
-  VERSION = "4.0.9.4"
+  VERSION = "4.0.9.5"
 
   @mutex = Mutex.new
 
--- lib/friendly_id/base.rb
+++ lib/friendly_id/base.rb
@@ -221,9 +221,9 @@ often better and easier to use {FriendlyId::Slugged slugs}.
       relation = relation_class.new(self, arel_table)
 
       if finder_needs_type_condition?
-        relation.where(type_condition).create_with(inheritance_column.to_sym => sti_name)
+        relation.with_deleted.where(type_condition).create_with(inheritance_column.to_sym => sti_name)
       else
-        relation
+        relation.with_deleted
       end
     end
 
